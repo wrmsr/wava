e4ebf7f0c4e6fff56638d5fa9fa3bc6847eb7c9c
wrmsr <timwilloney@gmail.com>
2016-09-28 01:28:31 -0700

    --

--- src/main/java/com/wrmsr/wava/clang/api/CxCursor.java
+++ src/main/java/com/wrmsr/wava/clang/api/CxCursor.java
@@ -43,4 +43,82 @@ public interface CxCursor
     int getNumArguments();
 
     CxCursor getArgument(int i);
+
+    int getNumTemplateArguments();
+
+    CxTemplateArgumentKind getTemplateArgumentKind(int i);
+
+    CxType getTemplateArgumentType(int i);
+
+    long getTemplateArgumentValue(int i);
+
+    long getTemplateArgumentUnsignedValue(int i);
+
+    boolean isMacroFunctionLike();
+
+    boolean isMacroBuiltin();
+
+    boolean isFunctionInlined();
+
+    CxString getDeclObjCTypeEncoding(CxCursor cursor);
+
+    CxType getCursorResultType(CxCursor cursor);
+
+    long getOffsetOfField(CxCursor cursor);
+
+    boolean isAnonymous(CxCursor cursor);
+
+    boolean isBitField(CxCursor cursor);
+
+    boolean isVirtualBase(CxCursor cursor);
+
+    CxCxxAccessSpecifier getCXXAccessSpecifier(CxCursor cursor);
+
+    CxStorageClass getStorageClass(CxCursor cursor);
+
+    int getNumOverloadedDecls(CxCursor cursor);
+
+    CxCursor getOverloadedDecl(CxCursor cursor, int index);
+
+    CxType getIBOutletCollectionType(CxCursor cursor);
+
+    CxString getCursorUSR(CxCursor cursor);
+
+    CxString getCursorSpelling(CxCursor cursor);
+
+    CxSourceRange getSpellingNameRange(CxCursor cursor, int pieceIndex, int options);
+
+    CxString getCursorDisplayName(CxCursor cursor);
+
+    CxCursor getCursorReferenced(CxCursor cursor);
+
+    CxCursor getCursorDefinition(CxCursor cursor);
+
+    boolean isCursorDefinition(CxCursor cursor);
+
+    CxCursor getCanonicalCursor(CxCursor cursor);
+
+    int getObjCSelectorIndex(CxCursor cursor);
+
+    boolean isDynamicCall(CxCursor cursor);
+
+    CxType getReceiverType(CxCursor cursor);
+
+    CxSourceRange getCommentRange(CxCursor cursor);
+
+    CxString getRawCommentText(CxCursor cursor);
+
+    CxString getBriefCommentText(CxCursor cursor);
+
+    CxString getMangling(CxCursor cursor);
+
+    CxModule getModule(CxCursor cursor);
+
+    CxCursorKind getTemplateCursorKind(CxCursor cursor);
+
+    CxCursor getSpecializedCursorTemplate(CxCursor cursor);
+
+    CxSourceRange getCursorReferenceNameRange(CxCursor cursor, int nameFlags, int pieceIndex);
+
+    CxEvalResult evaluate(CxCursor cursor);
 }
--- src/main/java/com/wrmsr/wava/clang/jffi/JffiCxCursor.java
+++ src/main/java/com/wrmsr/wava/clang/jffi/JffiCxCursor.java
@@ -17,7 +17,13 @@ import com.kenai.jffi.Type;
 import com.wrmsr.wava.clang.api.CxCursor;
 import com.wrmsr.wava.clang.api.CxCursorKind;
 import com.wrmsr.wava.clang.api.CxCursorVisitor;
+import com.wrmsr.wava.clang.api.CxCxxAccessSpecifier;
+import com.wrmsr.wava.clang.api.CxEvalResult;
+import com.wrmsr.wava.clang.api.CxModule;
+import com.wrmsr.wava.clang.api.CxSourceRange;
+import com.wrmsr.wava.clang.api.CxStorageClass;
 import com.wrmsr.wava.clang.api.CxString;
+import com.wrmsr.wava.clang.api.CxTemplateArgumentKind;
 import com.wrmsr.wava.clang.api.CxType;
 
 import static com.kenai.jffi.Array.newArray;
@@ -123,5 +129,239 @@ final class JffiCxCursor
     public CxCursor getArgument(int i)
     {
         return runtime.getLibClang().clang_Cursor_getArgument(this, i);
+    }
+
+    @Override
+    public int getNumTemplateArguments()
+    {
+        return runtime.getLibClang().clang_Cursor_getNumTemplateArguments(this);
+    }
+
+    @Override
+    public CxTemplateArgumentKind getTemplateArgumentKind(int i)
+    {
+        return runtime.getLibClang().clang_Cursor_getTemplateArgumentKind(this, i);
+    }
+
+    @Override
+    public CxType getTemplateArgumentType(int i)
+    {
+        return runtime.getLibClang().clang_Cursor_getTemplateArgumentType(this, i);
+    }
+
+    @Override
+    public long getTemplateArgumentValue(int i)
+    {
+        return runtime.getLibClang().clang_Cursor_getTemplateArgumentValue(this, i);
+    }
+
+    @Override
+    public long getTemplateArgumentUnsignedValue(int i)
+    {
+        return runtime.getLibClang().clang_Cursor_getTemplateArgumentUnsignedValue(this, i);
+    }
+
+    @Override
+    public boolean isMacroFunctionLike()
+    {
+        return runtime.getLibClang().clang_Cursor_isMacroFunctionLike(this);
+    }
+
+    @Override
+    public boolean isMacroBuiltin()
+    {
+        return runtime.getLibClang().clang_Cursor_isMacroBuiltin(this);
+    }
+
+    @Override
+    public boolean isFunctionInlined()
+    {
+        return runtime.getLibClang().clang_Cursor_isFunctionInlined(this);
+    }
+
+    @Override
+    public CxString getDeclObjCTypeEncoding(CxCursor cursor)
+    {
+        return runtime.getLibClang().clang_getDeclObjCTypeEncoding(this);
+    }
+
+    @Override
+    public CxType getCursorResultType(CxCursor cursor)
+    {
+        return runtime.getLibClang().clang_getCursorResultType(this);
+    }
+
+    @Override
+    public long getOffsetOfField(CxCursor cursor)
+    {
+        return runtime.getLibClang().clang_Cursor_getOffsetOfField(this);
+    }
+
+    @Override
+    public boolean isAnonymous(CxCursor cursor)
+    {
+        return runtime.getLibClang().clang_Cursor_isAnonymous(this);
+    }
+
+    @Override
+    public boolean isBitField(CxCursor cursor)
+    {
+        return runtime.getLibClang().clang_Cursor_isBitField(this);
+    }
+
+    @Override
+    public boolean isVirtualBase(CxCursor cursor)
+    {
+        return runtime.getLibClang().clang_isVirtualBase(this);
+    }
+
+    @Override
+    public CxCxxAccessSpecifier getCXXAccessSpecifier(CxCursor cursor)
+    {
+        return runtime.getLibClang().clang_getCXXAccessSpecifier(this);
+    }
+
+    @Override
+    public CxStorageClass getStorageClass(CxCursor cursor)
+    {
+        return runtime.getLibClang().clang_Cursor_getStorageClass(this);
+    }
+
+    @Override
+    public int getNumOverloadedDecls(CxCursor cursor)
+    {
+        return runtime.getLibClang().clang_getNumOverloadedDecls(this);
+    }
+
+    @Override
+    public CxCursor getOverloadedDecl(CxCursor cursor, int index)
+    {
+        return runtime.getLibClang().clang_getOverloadedDecl(this, index);
+    }
+
+    @Override
+    public CxType getIBOutletCollectionType(CxCursor cursor)
+    {
+        return runtime.getLibClang().clang_getIBOutletCollectionType(this);
+    }
+
+    @Override
+    public CxString getCursorUSR(CxCursor cursor)
+    {
+        return runtime.getLibClang().clang_getCursorUSR(this);
+    }
+
+    @Override
+    public CxString getCursorSpelling(CxCursor cursor)
+    {
+        return runtime.getLibClang().clang_getCursorSpelling(this);
+    }
+
+    @Override
+    public CxSourceRange getSpellingNameRange(CxCursor cursor, int pieceIndex, int options)
+    {
+        return runtime.getLibClang().clang_Cursor_getSpellingNameRange(this, pieceIndex, options);
+    }
+
+    @Override
+    public CxString getCursorDisplayName(CxCursor cursor)
+    {
+        return runtime.getLibClang().clang_getCursorDisplayName(this);
+    }
+
+    @Override
+    public CxCursor getCursorReferenced(CxCursor cursor)
+    {
+        return runtime.getLibClang().clang_getCursorReferenced(this);
+    }
+
+    @Override
+    public CxCursor getCursorDefinition(CxCursor cursor)
+    {
+        return runtime.getLibClang().clang_getCursorDefinition(this);
+    }
+
+    @Override
+    public boolean isCursorDefinition(CxCursor cursor)
+    {
+        return runtime.getLibClang().clang_isCursorDefinition(this);
+    }
+
+    @Override
+    public CxCursor getCanonicalCursor(CxCursor cursor)
+    {
+        return runtime.getLibClang().clang_getCanonicalCursor(this);
+    }
+
+    @Override
+    public int getObjCSelectorIndex(CxCursor cursor)
+    {
+        return runtime.getLibClang().clang_Cursor_getObjCSelectorIndex(this);
+    }
+
+    @Override
+    public boolean isDynamicCall(CxCursor cursor)
+    {
+        return runtime.getLibClang().clang_Cursor_isDynamicCall(this);
+    }
+
+    @Override
+    public CxType getReceiverType(CxCursor cursor)
+    {
+        return runtime.getLibClang().clang_Cursor_getReceiverType(this);
+    }
+
+    @Override
+    public CxSourceRange getCommentRange(CxCursor cursor)
+    {
+        return runtime.getLibClang().clang_Cursor_getCommentRange(this);
+    }
+
+    @Override
+    public CxString getRawCommentText(CxCursor cursor)
+    {
+        return runtime.getLibClang().clang_Cursor_getRawCommentText(this);
+    }
+
+    @Override
+    public CxString getBriefCommentText(CxCursor cursor)
+    {
+        return runtime.getLibClang().clang_Cursor_getBriefCommentText(this);
+    }
+
+    @Override
+    public CxString getMangling(CxCursor cursor)
+    {
+        return runtime.getLibClang().clang_Cursor_getMangling(this);
+    }
+
+    @Override
+    public CxModule getModule(CxCursor cursor)
+    {
+        return runtime.getLibClang().clang_Cursor_getModule(this);
+    }
+
+    @Override
+    public CxCursorKind getTemplateCursorKind(CxCursor cursor)
+    {
+        return runtime.getLibClang().clang_getTemplateCursorKind(this);
+    }
+
+    @Override
+    public CxCursor getSpecializedCursorTemplate(CxCursor cursor)
+    {
+        return runtime.getLibClang().clang_getSpecializedCursorTemplate(this);
+    }
+
+    @Override
+    public CxSourceRange getCursorReferenceNameRange(CxCursor cursor, int nameFlags, int pieceIndex)
+    {
+        return runtime.getLibClang().clang_getCursorReferenceNameRange(this, nameFlags, pieceIndex);
+    }
+
+    @Override
+    public CxEvalResult evaluate(CxCursor cursor)
+    {
+        return runtime.getLibClang().clang_Cursor_Evaluate(this);
     }
 }
